a2672dd20736f7ff61e70d9a836d998f16d8364d
Julius Petersson <julius@julius.pe>
2026-05-13 22:32:05 +0200

    MGS-1 cam-rel: camera-relative analog movement via GV_PadOrigin

--- source/game/freecam.c
+++ source/game/freecam.c
@@ -59,6 +59,7 @@ void FreeCam_Tick(void)
     const FreeCamRoomConfig *room = g_current_room;
     GV_PAD *pad;
     int     ots_active;
+    int     pad_origin;
     SVECTOR eye;
     SVECTOR center;
 
@@ -90,6 +91,9 @@ void FreeCam_Tick(void)
         center.vz = GM_PlayerPosition.vz + (short)((cos_h * OTS_LOOK_FORWARD) >> 12);
         center.vy = GM_PlayerPosition.vy + OTS_LOOK_HEIGHT;
         center.pad = 0;
+
+        /* Eye is behind Snake along his heading; cam-forward = heading. */
+        pad_origin = GM_PlayerHeading;
     }
     else
     {
@@ -114,6 +118,9 @@ void FreeCam_Tick(void)
         center = GM_PlayerPosition;
         center.vy += 200;
         center.pad = 0;
+
+        /* Eye sits at angle g_yaw around Snake; cam-forward = g_yaw + 180 deg. */
+        pad_origin = (g_yaw + 2048) & 0x0FFF;
     }
 
     if (eye.vx < room->bounds_min.vx) { eye.vx = room->bounds_min.vx; }
@@ -126,4 +133,6 @@ void FreeCam_Tick(void)
     gUnkCameraStruct2_800B7868.eye    = eye;
     gUnkCameraStruct2_800B7868.center = center;
     gUnkCameraStruct2_800B7868.zoom   = 320;
+
+    GV_OriginPadSystem(pad_origin);
 }
